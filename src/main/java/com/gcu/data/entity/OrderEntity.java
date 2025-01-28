3696c2c68ba44f5b77d38fa45e2006c0dba2e7f6
Saralyn Poole <144714241+spoole10@users.noreply.github.com>
2025-01-28 11:20:01 -0700

    Update OrderEntity.java

--- src/main/java/com/gcu/data/entity/OrderEntity.java
+++ src/main/java/com/gcu/data/entity/OrderEntity.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Table("orders")
+@Table("ORDERS")
 public class OrderEntity {
 	@Id
 	Long id;
